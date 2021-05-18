31c987f4e49a8f6f0b0dd35a24381737afb3923b
Felipe Arreola <arreolaf@oregonstate.edu>
2021-05-18 16:06:01 -0700

    added test for Class Producer- tested production method

--- test/producer_test.rb
+++ test/producer_test.rb
@@ -5,4 +5,31 @@ require_relative '../lib/producer'
 
 class ProducerTest < Minitest::Test
 
+  class ProvinceFake
+    attr_accessor :total_production
+
+    def initialize
+    @total_production = 0
+    end
+
+  end
+
+  producer_data = {
+    name: 'Byzantium', cost: 10, production: 9
+  }
+
+  producer_obj = Producer.new(ProvinceFake.new, producer_data)
+
+  describe "Class-Producer: Member function(s)-" do
+  
+    it "has 'production=' function:" do
+      def test_producer_production
+        producer_obj.production
+        assert_equal(9, producer_obj.province.total_production)
+      end
+    end
+
+  end
+  #END- Member Functions
+
 end
